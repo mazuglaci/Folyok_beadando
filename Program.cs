d87ef2c3e8a957b404f106827dcb98bfacade22d
zsinkohajnalka <83495475+zsinkohajnalka@users.noreply.github.com>
2021-05-16 18:25:56 +0200

    main javítás

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace Folyok_beadando {
     class Program {
         static void Main(string[] args) {
-            //Afeladat.AMegoldas();
+            Console.WriteLine("Az első feladat megoldása: ");
+            Afeladat.AMegoldas();
+            Console.WriteLine("A második feladat megoldása: ");
             Bfeladat.BMegoldas();
             Console.ReadKey();
         }
